dbdb7798526b933e73442bfcd043360bfdc8a001
Keval Maisuriya <kevalmaisuriyaelsner@gmail.com>
2022-06-23 16:13:30 +0530

    Call Hello.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 import './App.css';
 
 function App() {
   return (
 
     <div className="App">
-      <Greet/>
-      <Welcome/>
+      {/* <Greet/>
+      <Welcome/> */}
+      <Hello/>
     </div>
   );
 }
