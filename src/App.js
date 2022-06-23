d78abccc4c4dde57e45fc01e6c9724b40190fe1f
Keval Maisuriya <kevalmaisuriyaelsner@gmail.com>
2022-06-23 15:24:41 +0530

    Call Greet Function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import Greet from './components/Greet';
 import './App.css';
 
 function App() {
   return (
 
     <div className="App">
-            
+      <Greet/>
     </div>
   );
 }
