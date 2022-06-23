33c96a5fea1d233ddae109bff0f3ed97a8c46fe1
Keval Maisuriya <kevalmaisuriyaelsner@gmail.com>
2022-06-23 15:32:31 +0530

    Use fat arrow function

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,10 @@
 import React from "react";
 
-function Greet() {
-    return <h1>Hello Keval!</h1>
-}
+// function Greet() {
+//     return <h1>Hello Keval!</h1>
+// }
+
+//Using fat arrow function
+const Greet = () => <h1>Hello Keval</h1>
 
 export default Greet
